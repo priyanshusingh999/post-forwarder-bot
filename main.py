5a397d9be11aaed3f18eacd25303904200c79aee
Priyanshu Singh Rajput <147338546+priyanshusingh999@users.noreply.github.com>
2025-04-30 10:23:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def home():
     return 'Bot is running!', 200
 
 def run_flask():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8090)
 
 threading.Thread(target=run_flask).start()
 
