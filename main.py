fdfdcfe64d8fcc505b4e0433701d4ad10c33c402
Priyanshu Singh Rajput <147338546+priyanshusingh999@users.noreply.github.com>
2025-04-30 10:49:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def home():
     return '🤖 Bot is Running! Created By @devx_coder("priyanshusingh999")'
 
 def run_flask():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8090)
 
 threading.Thread(target=run_flask).start()
 
