02c8b631d7a97dccceb18a921786fb1b5eaac39e
Priyanshu Singh Rajput <147338546+priyanshusingh999@users.noreply.github.com>
2025-05-14 20:43:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 # Config Variables from environment or fallback
 TOKEN = os.getenv('TOKEN')
-OWNER_ID = int(os.getenv('OWNER_ID'))
+OWNER_ID = os.getenv('OWNER_ID')
 FORCE_SUB_CHANNEL = os.getenv('FORCE_SUB_CHANNEL')
